8d32d2e67537052e9d3ea6cc5b1f70299f17273e
Shrooblord <shrooblord@thejaycode.com>
2023-02-07 20:56:31 +0100

    use new getLicenses function and optimise main license loop

--- mods/MoveUI/data/scripts/entity/merchants/scrapyard.lua
+++ mods/MoveUI/data/scripts/entity/merchants/scrapyard.lua
@@ -16,33 +16,31 @@ if ScrapyardPlus then
     function Scrapyard.updateMoveUILicenses(timeStep, scrapyardFactionIndex)
         local Data, licenses
 
-        if Scrapyard.getData then
-            --print("\ngetting Scrapyard data for faction " .. scrapyardFactionIndex .. "...")
-            Data = Scrapyard.getData(scrapyardFactionIndex)
-            licenses = {}
-            for d in ipairs(Data) do
-                --print("index: ".. d)
-                --print("data: " .. serialize(Data[d]))
-                --print("facId: " .. Data[d].factionIndex)
-                --print("license: " .. Data[d].license)
-                licenses[Data[d].factionIndex] = Data[d].license
-            end
+        --print("\ngetting Scrapyard data for faction " .. scrapyardFactionIndex .. "...")
+        Data = Scrapyard.getLicenses(scrapyardFactionIndex)
+        licenses = {}
+        for d in ipairs(Data) do
+            --print("index: ".. d)
+            --print("data: " .. serialize(Data[d]))
+            --print("facId: " .. Data[d].factionIndex)
+            --print("license: " .. Data[d].license)
+            licenses[Data[d].factionIndex] = Data[d].license
+        end
 
-            local x,y = Sector():getCoordinates()
-            for factionIndex,duration in pairs(licenses) do
-                local faction = Faction(factionIndex)
+        local x,y = Sector():getCoordinates()
+        for factionIndex,duration in pairs(licenses) do
+            local faction = Faction(factionIndex)
 
-                if faction.isPlayer or faction.isAlliance then
-                    -- read current or init new
-                    local pLicenses = Scrapyard.GetFactionLicense(factionIndex)
-                    local time = round(duration - timeStep)
-                    if time <= 0 then
-                    time = nil
-                    end
-                    pLicenses[x][y] = time
-
-                    faction:setValue("MoveUI#Licenses", serialize(pLicenses))
+            if faction and (faction.isPlayer or faction.isAlliance) then
+                -- read current or init new
+                local pLicenses = Scrapyard.GetFactionLicense(factionIndex)
+                local time = round(duration - timeStep)
+                if time <= 0 then
+                time = nil
                 end
+                pLicenses[x][y] = time
+
+                faction:setValue("MoveUI#Licenses", serialize(pLicenses))
             end
         end
     end
